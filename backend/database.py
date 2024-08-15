a803f3629fd7b4ae482331cdd135d18b2e020c80
4YU5H25 <ayush4yush@gmail.com>
2024-08-15 23:06:05 +0530

    changes

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:root@localhost/url_shortener"
+# DATABASE_URL = "postgresql://postgres:root@localhost/url_shortener"
+DATABASE_URL = "postgresql://url_shortener_25ps_user:InjfclxQNp3snhATCjEFICSruJN9qHrr@dpg-cqv3l5rtq21c73a1jbk0-a.singapore-postgres.render.com/url_shortener_25ps"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
